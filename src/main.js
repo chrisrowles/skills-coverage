f8cf3709235873307487d00f6e8c45afff271444
Chris Rowles <chris.rowles@godeploy.com>
2021-05-09 03:19:35 +0100

    async

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import './styles/main.css'
 
 import api from './api'
 import calculate from './calculate'
+import notify from './notify'
 
 window._api = api
 window._calculate = calculate
+window._notify = notify
