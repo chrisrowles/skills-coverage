90b4b124862042b15c086552f2af6f99342b805b
Chris Rowles <chris.rowles@godeploy.com>
2021-05-09 01:53:00 +0100

    build the calculator

---
+++ src/calculate.js
@@ -0,0 +1,35 @@
+const calculate = {}
+
+calculate.overallCoverageForSkill = (users, skillName) => {
+    let skillName = "dotnet"
+    let overallLevel = 0
+    let numPeopleAtMinimumLevel = 0
+    let minimumLevel = 0
+    let requiredAtMinimumLevel = 0
+
+    users.forEach(user => {
+        try {
+            let skill = user.skills.find(s => s.name === skillName)
+            minimumLevel = skill.minimumLevel
+            requiredAtMinimumLevel = skill.requiredAtMinimumLevel
+    
+            let userLevel = skill.user_skill.level
+            overallLevel += userLevel
+    
+            if (userLevel >= minimumLevel) {
+                ++numPeopleAtMinimumLevel
+            }
+        } catch (err) {
+            return err
+        }
+    })
+
+    return {
+        skill: skillName,
+        numPeopleAtMinimumLevel: numPeopleAtMinimumLevel,
+        numPeopleRequiredAtMinimumLevel: requiredAtMinimumLevel,
+        overallLevel: overallLevel, // dunno if this is really useful for anything...
+        coverageAchieved: (numPeopleAtMinimumLevel >= requiredAtMinimumLevel) ? true : false,
+        coveragePercent: Math.round(numPeopleAtMinimumLevel / requiredAtMinimumLevel * 100)
+    }
+}
